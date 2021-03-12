e688dc5ac40b6b7b37588cac278ab9423d7c82f9
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-12 15:29:07 +0100

    add. complete left effect

--- main.js
+++ main.js
@@ -6,43 +6,80 @@ $ ( function(){
   $(".next").click(
     function(){
 
+      // dichiarazione variabili
       var imgSlider = $(".images img.active");
+
+      var bulletSlider = $(".nav i.active");
+
 
       var firstImg = $(".images img.first");
 
+      var firstBullet = $(".nav i.first");
+
+      // rimuovo classe active al click
+
       imgSlider.removeClass("active");
 
-      if(imgSlider.hasClass("last") == false){
+      bulletSlider.removeClass("active");
 
+      // se non è presente classe last aggiungi active al successivo
+
+      if(imgSlider.hasClass("last") == false && bulletSlider.hasClass("last") == false){
 
         imgSlider.next().addClass("active");
 
+        bulletSlider.next().addClass("active");
+
       } else{
 
+        // altrimenti aggiungi active all'elemento con classe first
+
         firstImg.addClass("active");
+
+        firstBullet.addClass("active");
 
       }
 
     }
   );
 
+  // evento al click sx
+
   $(".prev").click(
     function(){
 
+      // dichiarazione variabili
+
       var imgSlider = $(".images img.active");
+
+      var bulletSlider = $(".nav i.active");
 
       var lastImg = $(".images img.last");
 
+      var lastBullet = $(".nav i.last");
+
+      // al click rimuovo classe active
+
       imgSlider.removeClass("active");
 
-      if(imgSlider.hasClass("first") == false){
+      bulletSlider.removeClass("active");
+
+      // se non è presente classe first aggiungo active all'elemento precedente
+
+      if(imgSlider.hasClass("first") == false && bulletSlider.hasClass("first") == false ){
 
 
         imgSlider.prev().addClass("active");
 
+        bulletSlider.prev().addClass("active");
+
       } else{
 
+        // altrimenti aggiungo active all'elemento che ha classe last
+
         lastImg.addClass("active");
+
+        lastBullet.addClass("active");
 
       }
 
