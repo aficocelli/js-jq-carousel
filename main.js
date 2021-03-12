b8338391b5823623866931cb7f4358996ccdc8a9
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-12 14:58:49 +0100

    add. effect

--- main.js
+++ main.js
@@ -2,14 +2,28 @@
 
 $ ( function(){
 
-  // evento al click
+  // evento al click dx
   $(".next").click(
+    function(){
+
+      var imgSlider = $(".images img.active");
+      imgSlider.removeClass("active");
+
+      imgSlider.next().addClass("active");
+      
+
+    }
+  );
+
+  $(".prev").click(
     function(){
 
       alert("ciao");
 
     }
   );
+
+
 
 
 
